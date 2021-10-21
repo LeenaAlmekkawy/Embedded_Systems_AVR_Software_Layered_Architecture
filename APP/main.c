ce44716df9e65a33f73535e5b8651aea944e7885
LeenaAlmekkawy <75577712+LeenaAlmekkawy@users.noreply.github.com>
2021-10-21 19:41:54 +0200

    add interrupt application

--- APP/main.c
+++ APP/main.c
@@ -2,24 +2,28 @@
 #include"../LIB/BIT_MATH.h"
 
 #include"../MCAL/DIO/DIO_interface.h"
+#include"../MCAL/EXTI/EXTI_interface.h"
+#include"../MCAL/GIE/GIE_interface.h"
 #include"../HAL/LCD/LCD_interface.h"
 #include"../HAL/KPD/KPD_interface.h"
 #include <util/delay.h>
 
 void main(void)
-{
+{    DIO_voidSetPinDirection(PORTA,PIN1,OUTPUT);
+DIO_voidSetPinDirection(PORTD,PIN2,INPUT);
+DIO_voidSetPinValue(PORTD,PIN2,HIGH);
+	EXTI_voidSetInt0SenseControl(FALLING_EDGE);
+	EXTI_voidINT0Control(ENABLE );
+	GIE_voidEnableGlobalInterrupt();
 
-	DIO_voidSetPinDirection(PORTA ,PIN0,OUTPUT);
-	while(1)
-	{   DIO_voidSetPinValue(PORTA ,PIN0,HIGH);
-	    _delay_ms(3000);
+ while(1)
+ {
 
-		DIO_voidSetPinValue(PORTA ,PIN0,LOW);
-		 _delay_ms(1000);
-	}
-
+ }
 
 }
+
+
 
 
 
