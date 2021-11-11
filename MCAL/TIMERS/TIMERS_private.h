0f76ce9fd6c5e7615c0e37abc9010e8b57c201cd
LeenaAlmekkawy <75577712+LeenaAlmekkawy@users.noreply.github.com>
2021-11-11 17:14:59 +0200

    Update TIMERS_private.h

--- MCAL/TIMERS/TIMERS_private.h
+++ MCAL/TIMERS/TIMERS_private.h
@@ -23,6 +23,28 @@
 #define TIFR_TOV0    0
 #define TIFR_OCF0    1
 
+
+
+#define TCCR2       *((volatile u8*)0x45)
+#define TCCR2_CS20   0
+#define TCCR2_CS21   1
+#define TCCR2_CS22   2
+#define TCCR2_WGM21  3
+#define TCCR2_COM20  4
+#define TCCR2_COM21  5
+#define TCCR2_WGM20  6
+#define TCCR2_FOC2   7
+
+#define TCNT2        *((volatile u8*)0x44)
+
+#define OCR2         *((volatile u8*)0x43)
+
+#define TIMSK_TOIE2  6
+#define TIMSK_OCIE2  7
+
+#define TIFR_TOV2    6
+#define TIFR_OCF2    7
+
 #define NULL    (void *)0
 
 #endif
