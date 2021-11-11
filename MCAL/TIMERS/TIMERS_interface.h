89ab192819ee84dd3e7b73530d2a2913ceab97ef
LeenaAlmekkawy <75577712+LeenaAlmekkawy@users.noreply.github.com>
2021-11-11 17:14:23 +0200

    Update TIMERS_interface.h

--- MCAL/TIMERS/TIMERS_interface.h
+++ MCAL/TIMERS/TIMERS_interface.h
@@ -5,12 +5,14 @@ void TIMERS_voidInit(void);
 
 void TIMERS_voidTimer0SetCompareMatchValue(u8 Copy_u8CompareMatchValue);
 
+void TIMERS_voidTimer2SetCompareMatchValue(u8 Copy_u8CompareMatchValue);
+
 void TIMERS_voidTimer0SetPreLoadValue(u8 Copy_u8PreLoadValue);
+
+void TIMERS_voidTimer2SetPreLoadValue(u8 Copy_u8PreLoadValue);
 
 void TIMERS_voidTimer0CTCSetCallBackFunction(void(*Copy_pvCTCFunction)(void));
 
-void Timer_voidCounter(void);
-
-void TIMERS_voidSetBusyWait(u16 Copy_u16Time_ms);
+void TIMERS_voidSetBusyWaitTimer2_ms(u32 Copy_u32Time_ms);
 
 #endif
